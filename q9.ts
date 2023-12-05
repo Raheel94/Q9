47abab9a7a807c79f9469335cbf3e8b0d99ea426
Raheel94 <muha.raheel94@gmail.com>
2023-12-05 12:51:55 +0500

    go

---
+++ q9.ts
@@ -0,0 +1,5 @@
+// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+let number = 5
+let message: string = `My favorite number is ${number}`;
+console.log(message)
